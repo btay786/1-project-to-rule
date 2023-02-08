c945a161521a847649dca536bb6fd6400cb5b330
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-07 20:05:46 -0800

    Modifiy the string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 var pageNumber = document.getElementById('page-number');
-
+var isbnKey = "ISBN:9780980200447";
 // https://openlibrary.org/api/books?bibkeys={ISBN:9780980200447}&jscmd=data&format=json
 // Must be "ISBN:00000000000000"
 var getPageNumber = function(){
-    var apiUrl = "https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json";
+    var apiUrl = "https://openlibrary.org/api/books?bibkeys="+isbnKey+"&jscmd=data&format=json";
     fetch(apiUrl)
         
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            console.log("page number: " + data["ISBN:9780980200447"].number_of_pages);
+            console.log("page number: " + data[isbnKey].number_of_pages);
             var total = document.createElement('h2'); // create a paragraph
 
-            total.textContent = "number of page is " + data["ISBN:9780980200447"].number_of_pages;
+            total.textContent = "number of page is " + data[isbnKey].number_of_pages;
             pageNumber.appendChild(total);
         });
     console.log(apiUrl)
