cd58b8ccbcf3959bba190664924aaa889a39f193
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-02-09 21:12:18 -0800

    comment out code and add a card to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,7 @@ $(document).ready(function () {  // only begin once page has loaded
                             var total = document.createElement('h2'); // create a paragraph
                             
                             total.textContent = "number of page is " + data[isbnKey].number_of_pages;
-                            pageNumber.appendChild(total);
+                            //pageNumber.appendChild(total);
                         });
                     console.log(apiUrl)
                 }
@@ -107,7 +107,7 @@ $(document).ready(function () {  // only begin once page has loaded
                         pageNumber.appendChild(isbnNum);
                     }
                 };
-               // getPageNumber();
+                getPageNumber();
                 //checkISBN(apiUrl);
                 console.log(isbnKey);
             },
@@ -134,41 +134,41 @@ $(document).ready(function () {  // only begin once page has loaded
 
 
 
-$(document).ready(function () {  // only begin once page has loaded
-    $("#txtBookSearch").autocomplete({ // attach auto-complete functionality to textbox
+//$(document).ready(function () {  // only begin once page has loaded
+    //$("#txtBookSearch").autocomplete({ // attach auto-complete functionality to textbox
         // define source of the data
-        source: function (request, response) {
+        //source: function (request, response) {
             // url link to google books, including text entered by user (request.term)
-            var booksUrl = "https://www.googleapis.com/books/v1/volumes?printType=books&q=" + encodeURIComponent(request.term);
-            $.ajax({
-                url: booksUrl,
-                dataType: "jsonp",
-                success: function(data) {
-                    response($.map(data.items, function (item) {
-                        if (item.volumeInfo.authors && item.volumeInfo.title && item.volumeInfo.industryIdentifiers && item.volumeInfo.publishedDate)
-                        {
-                            return {
+            //var booksUrl = "https://www.googleapis.com/books/v1/volumes?printType=books&q=" + encodeURIComponent(request.term);
+            //$.ajax({
+                //url: booksUrl,
+                //dataType: "jsonp",
+                //success: function(data) {
+                    //response($.map(data.items, function (item) {
+                        //if (item.volumeInfo.authors && item.volumeInfo.title && item.volumeInfo.industryIdentifiers && item.volumeInfo.publishedDate)
+                        //{
+                            //return {
                                 // label value will be shown in the suggestions
-                                label: item.volumeInfo.title + ', ' + item.volumeInfo.authors[0] + ', ' + item.volumeInfo.publishedDate,
+                                //label: item.volumeInfo.title + ', ' + item.volumeInfo.authors[0] + ', ' + item.volumeInfo.publishedDate,
                                 // value is what gets put in the textbox once an item selected
-                                value: item.volumeInfo.title,
+                                //value: item.volumeInfo.title,
                                 // other individual values to use later
-                                title: item.volumeInfo.title,
-                                author: item.volumeInfo.authors[0],
-                                isbn: item.volumeInfo.industryIdentifiers,
-                                publishedDate: item.volumeInfo.publishedDate,
-                                image: (item.volumeInfo.imageLinks == null ? "" : item.volumeInfo.imageLinks.thumbnail),
-                                description: item.volumeInfo.description,
-                            };
-                        }
-                    }));
-                }
-            });
-        },
-        select: function (event, ui) {
+                                //title: item.volumeInfo.title,
+                                //isbn: item.volumeInfo.industryIdentifiers,
+                                //author: item.volumeInfo.authors[0],
+                               // //publishedDate: item.volumeInfo.publishedDate,
+                                //image: (item.volumeInfo.imageLinks == null ? "" : item.volumeInfo.imageLinks.thumbnail),
+                                //description: item.volumeInfo.description,
+                            //};//
+                        //}//
+                    //}));//
+                //}//
+            //});//
+        //},//
+        //select: function (event, ui) {
             // what to do when an item is selected
             // first clear anything that may already be in the description
-            $('#divDescription').empty();
+            //$('#divDescription').empty();
             // we get the currently selected item using ui.item
             // show a pic if we have one
             // if (item.image != '')
@@ -176,21 +176,21 @@ $(document).ready(function () {  // only begin once page has loaded
             //     $('#divDescription').append('<img src="' + ui.item.image + '" style="float: left; padding: 10px;">');
             // }
             // and title, author, and year
-            $('#divDescription').append('<p><b>Title:</b> ' + ui.item.title  + '</p>');
-            $('#divDescription').append('<p><b>Author:</b> ' + ui.item.author  + '</p>');
-            $('#divDescription').append('<p><b>First published year:</b> ' + ui.item.publishedDate  + '</p>');          
+            //$('#divDescription').append('<p><b>Title:</b> ' + ui.item.title  + '</p>');
+            //$('#divDescription').append('<p><b>Author:</b> ' + ui.item.author  + '</p>');
+            //$('#divDescription').append('<p><b>First published year:</b> ' + ui.item.publishedDate  + '</p>');          
             // and the usual description of the book
-            $('#divDescription').append('<p><b>Description:</b> ' + ui.item.description  + '</p>');
+            //$('#divDescription').append('<p><b>Description:</b> ' + ui.item.description  + '</p>');
             // and show the link to oclc (if we have an isbn number)
-            if (ui.item.isbn && ui.item.isbn[0].identifier)
-            {
-                $('#divDescription').append('<P><b>ISBN:</b> ' + ui.item.isbn[0].identifier + '</p>');
-                $('#divDescription').append('<a href="http://www.worldcat.org/isbn/' + ui.item.isbn[0].identifier + '" target="_blank">View item on worldcat</a>');
-            }
-        },
-        minLength: 5 // set minimum length of text the user must enter
-    });
-});
+            //if (ui.item.isbn && ui.item.isbn[0].identifier)
+            //{
+                //$('#divDescription').append('<P><b>ISBN:</b> ' + ui.item.isbn[0].identifier + '</p>');
+                //$('#divDescription').append('<a href="http://www.worldcat.org/isbn/' + ui.item.isbn[0].identifier + '" target="_blank">View item on worldcat</a>');
+            //}
+        //},
+        //minLength: 5 // set minimum length of text the user must enter
+    //});//
+//});///
 
 var apiUrl = "https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json";
 
